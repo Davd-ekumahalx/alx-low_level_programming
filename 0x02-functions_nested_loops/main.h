4fc4b9b4ac3b629520a2320fcf1e3276e1f7c63f
Davd-ekumahalx <davidekumah21@gmail.com>
2023-10-11 23:33:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,17 @@
 #ifndef MAIN_H
 #ifdef MAIN_H
+{
 int _putchar(char c);
 int main(void);
 void print_alphabet(void);
 oid print_alphabet_x10(void);
 int _islower(int c);
- int _isalpha(int c);
- int print_sign(int n);
- int print_last_digit(int);
- void jack_bauer(void);
- void times_table(void);
- int add(int, int);
- void print_to_98(int n);
-
+int _isalpha(int c);
+int print_sign(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+}
 #endif
