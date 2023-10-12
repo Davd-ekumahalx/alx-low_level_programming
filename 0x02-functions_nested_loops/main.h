a0d75fa0a8260ee9237ed3e397f7644ae153432c
Davd-ekumahalx <davidekumah21@gmail.com>
2023-10-12 01:41:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H
-#ifdef MAIN_H
-{
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
 int _putchar(char c);
 int main(void);
 void print_alphabet(void);
-oid print_alphabet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -13,5 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-}
+
 #endif
