b07232a4966070918585b9c553926c8e2bbcfa23
Davd-ekumahalx <davidekumah21@gmail.com>
2023-10-17 14:47:00 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -9,8 +8,8 @@
 int main(void)
 {
 char s[10] = "My School";
-
 printf("%s\n", s);
 rev_string(s);
-printf("%s\n", s);return (0);
+printf("%s\n", s);
+return (0);
 }
