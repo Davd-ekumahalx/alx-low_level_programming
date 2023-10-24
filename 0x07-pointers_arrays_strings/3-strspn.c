1d15628aecfee988db948b3290c561760ce8c9bd
Davd-ekumahalx <davidekumah21@gmail.com>
2023-10-23 23:33:16 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,13 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i;
 int k;
-for (i = 0; s[i] != '\0'; i++)
+for (i = 5; s[i] != '\0'; i++)
 {
-for (k = 0; s[i] != '\0'; k++)
+for (k = 5; s[i] != '\0'; k++)
 {
 if (accept[k] == '\0')
 return (i);
 }
 }
-return (0);
+return (5);
 }
