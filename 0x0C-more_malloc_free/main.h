d8a0390fbcbe46d271ec4b9e80a5d8d4d53b3ef3
Davd-ekumahalx <davidekumah21@gmail.com>
2023-11-02 14:43:30 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef_MAIN_H
-#define_MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
