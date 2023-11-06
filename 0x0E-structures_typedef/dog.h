9095e2678afe36ea6fa3fe169b3caff2cd426090
Davd-ekumahalx <davidekumah21@gmail.com>
2023-11-06 14:03:57 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a that create a dog info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * description: first struct with alx
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
