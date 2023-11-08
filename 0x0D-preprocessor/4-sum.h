37a036a87e3c7cbf68938d291c4e28a1aef01b37
Davd-ekumahalx <davidekumah21@gmail.com>
2023-11-07 22:16:46 -0800

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,12 @@
 #ifndef SUM_H
 #define SUM_H
-
-#define SUM(x, y) ((X) + (y))
+/**
+ * File: 4-sum
+ * @x: first int input
+ * @y: second int input
+ * Desc: sum up x and y
+ */
+#define SUM(x, y) (X + y)
 
 #endif
 
