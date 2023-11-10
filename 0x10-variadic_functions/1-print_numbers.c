79211b6aa7cfa537bd41cdb6303148c4f9254b9c
Davd-ekumahalx <davidekumah21@gmail.com>
2023-11-09 20:11:47 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ for (i = 0; i < n; i++)
 {
 arr = va_arg(ptr, const unsigned int);
 printf("%d", arr);
-if (i != (n == 1) && separator != NULL)
+if (i != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
